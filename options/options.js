9c801a3b1dbb768aace398f9a509ab8c2e841ad9
Crowdbotics <team@crowdbotics.com>
2024-02-09 19:58:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-thumb-club-46193",
+  url: "https://green-thumb-club-46193.botics.co",
+  api: "https://green-thumb-club-46193.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
